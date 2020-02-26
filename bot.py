cb344580cfb2d7b3630bcb5666b53b78a508ced0
Jonah Monaghan <jonah.james.monaghan@gmail.com>
2020-02-26 06:48:52 +0000

    -Added Player Class with Getters and Setters
    -Added New Commands
    *join
    *getplayers
    *holds
    *table
    
    -Heavy improvements to original roll, keep, check commands
    *Keeping a die will now remove it from the rollable dice pool
    *Check command separated to the holds and table command
    
    -Most commands have player checking so players who aren't in the game can't interact with the game

--- bot.py
+++ bot.py
@@ -5,6 +5,8 @@
 import os
 import discord
 import random
+import playerClass
+
 from discord.ext import commands
 from dotenv import load_dotenv
 
@@ -15,6 +17,7 @@ GUILD = os.getenv('GUILD')
 bot = commands.Bot(command_prefix='!')
 
 dice = [1, 1, 1, 1, 1]
+players = []
 
 
 @bot.event
@@ -25,44 +28,125 @@ async def on_ready():
 
 @bot.command()
 async def roll(ctx):
-    strSend = "You rolled:\n"
-    dice.clear()
-    for i in range(1, 6):
-        dice.append(random.randrange(1, 7))
-        strSend += "#" + str(i) + ": " + str(dice[i - 1]) + "\n"
-    strSend += "Which dice would you like to keep?"
+    rollerName = ctx.message.author.display_name
+    roller = None
+    for player in players:
+        if player.name == rollerName:
+            roller = player
+            break
+    if roller != None:
+      if roller.GetRollCount() > 0:
+        if(roller.GetChosen() == True):
+            roller.MakeRoll()
+            dice.clear()
+            strSend = roller.name + " rolled:\n"
+            for i in range(1, roller.GetRemainingDice()):
+              tmpRoll = random.randrange(1, 7)
+              dice.append(tmpRoll)
+              roller.SetTableDice(tmpRoll)
+              strSend += "#" + str(i) + ": " + str(dice[i - 1]) + "\n"
+            strSend += "Which dice would you like to keep?"
+        else:
+          strSend = "Don't get hasty! You need to choose from your last roll!"
+      else:
+        strSend = "You have reached your roll limit!"
+    else:
+      strSend = ":x:" + rollerName + " is not a player:x:"
     await ctx.send(strSend)
 
 
 @bot.command()
-async def mydice(ctx):
-    strSend = str(dice)
-    await ctx.send(strSend)
+async def holds(ctx):
+    sendStr = ""
+    holderName = ctx.message.author.display_name
+    holderMention = ctx.message.author.mention
+    holder = None
+    for player in players:
+        if player.name == holderName:
+            holder = player
+            break
+    if holder != None:
+      sendStr = holderMention + " you are holding:\n"
+      sendStr += str(holder.GetHoldDice())
+    else:
+      sendStr = ":x:" + holderName + " is not a player:x:"
+    await ctx.send(sendStr)
 
+@bot.command()
+async def table(ctx):
+    sendStr = ""
+    holderName = ctx.message.author.display_name
+    holderMention = ctx.message.author.mention
+    holder = None
+    for player in players:
+        if player.name == holderName:
+            holder = player
+            break
+    if holder != None:
+      sendStr = holderMention + " your table has:\n"
+      sendStr += str(holder.GetTableDice())
+    else:
+      sendStr = ":x:" + holderName + " is not a player:x:"
+    await ctx.send(sendStr)
 
 @bot.command()
 async def keep(ctx, arg, die2=0, die3=0, die4=0, die5=0):
-    
+    chooserName = ctx.message.author.display_name
+    chooser = None
+    for player in players:
+        if player.name == chooserName:
+            chooser = player
+            break
     try:
-      arg = int(arg)
+        arg = int(arg)
     except ValueError:
-      print(f'Cannot convert to int')
-    
+        print(f'Cannot convert to int')
+
     if isinstance(arg, str):
         if str(arg).lower() == "all":
             #Disable rolling, enable scoring
+            for value in chooser.GetTableDice():
+              chooser.SetHoldDice(value)
+              chooser.SetChosen(True)
             print(f'All dice saved')
         elif str(arg).lower() == "none":
+            chooser.SetChosen(True)
             print(f'No dice saved')
         else:
             await ctx.send(":x:That is not a valid parameter:x:")
     elif isinstance(arg, int):
         print(f'Its a number!')
-        if (arg > 0 and arg < 7):
-            #Change the roll range
+        if (arg > 0 and arg < chooser.GetRemainingDice()):
+            tmpTableDice = chooser.GetTableDice()
+            chooser.SetHoldDice(tmpTableDice[arg - 1])
             print(f'Dice number ' + str(arg) + ' saved')
         else:
             await ctx.send(":x:That is not a valid parameter:x:")
+
+
+@bot.command()
+async def join(ctx):
+    name = ctx.message.author.display_name
+    isPlaying = False
+    for player in players:
+        if player.name == name:
+            isPlaying = True
+            await ctx.send(":x:This user is already playing:x:")
+            break
+    if isPlaying == False: 
+      players.append(playerClass.Player(name))
+      await ctx.send(name + " has joined the battle!")
+
+
+@bot.command()
+async def getplayers(ctx):
+    strSend = "The current players are:\n"
+    if len(players) > 0:
+        for player in players:
+            strSend += player.name + "\n"
+    else:
+        strSend = "There are no players in this game."
+    await ctx.send(strSend)
 
 
 bot.run(TOKEN)
---
+++ playerClass.py
@@ -0,0 +1,52 @@
+#playerClass.py
+#A class that turns a discord user into the player of a Yahtzee
+#Author: Jonah Monaghan
+#02-25-2020
+
+class Player:
+
+  def __init__(self, username):
+    self.name = username #The player's name
+    self.rollCounter = 3 #Available rolls
+    self.dice = [] #A list for the player's held dice
+    self.tableDice = [] #A list for the player's table dice
+    self.remainingDice = 6 #Range is EXCLUSIVE so there are only 5 dice
+    self.chosen = True #If the player has chosen since their last roll
+  
+
+
+#Dice Functions
+  def SetHoldDice(self, value): #A function that adds the value of the chosen die
+    self.dice.append(value)
+    self.remainingDice = self.remainingDice - 1
+
+  def GetHoldDice(self):
+    return self.dice
+
+  def SetTableDice(self, value):
+    self.tableDice.append(value)
+
+  def GetTableDice(self):
+    return self.tableDice
+  
+  def GetRemainingDice(self):
+    return self.remainingDice
+  
+  def SetChosen(self, tf):
+    self.chosen = tf
+  
+  def GetChosen(self):
+    return self.chosen
+
+#Roll Counter Functions 
+  def MakeRoll(self):
+    self.rollCounter = self.rollCounter - 1
+  
+  def GetRollCount(self):
+    return int(self.rollCounter)
+
+  def ResetRolls(self):
+    self.rollCounter = 3
+    self.dice.clear()
+
+#Scoring Functions
