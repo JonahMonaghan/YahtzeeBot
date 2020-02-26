fc69d39d4712140512afe28d08b0a3ce6f9bb179
Jonah Monaghan <jonah.james.monaghan@gmail.com>
2020-02-26 21:36:33 +0000

    Added docstrings and comments, increased readability.

--- bot.py
+++ bot.py
@@ -1,87 +1,145 @@
 #bot.py
 #Initial code by: https://realpython.com/how-to-make-a-discord-bot-python/
 #Author: Jonah Monaghan
-# 02-25-2020
+#02-26-2020
+
+#Imports
 import os
 import discord
 import random
 import playerClass
 
+
+#Import froms
 from discord.ext import commands
 from dotenv import load_dotenv
 
-load_dotenv()
-TOKEN = os.getenv('TOKEN')
-GUILD = os.getenv('GUILD')
+#.env file
+load_dotenv() #Load in the .env file
+TOKEN = os.getenv('TOKEN') #Get the token from .env file
+GUILD = os.getenv('GUILD') #Get the connected guild
 
-bot = commands.Bot(command_prefix='!')
+bot = commands.Bot(command_prefix='!') #Set the bots command prefix to !
 
-dice = [1, 1, 1, 1, 1]
-players = []
+dice = [1, 1, 1, 1, 1] #Create list of dice
+players = [] #Create list of players
 
-
+#Once the bot has connected to the server provide feeback
 @bot.event
 async def on_ready():
     guild = discord.utils.find(lambda g: g.name == GUILD, bot.guilds)
     print(f'{bot.user} has connected to:\n' f'{guild.name}(id: {guild.id})')
 
-
+#When the player rolls
 @bot.command()
 async def roll(ctx):
+    """A command that rolls the available dice for the player
+    
+    This command will check how many dice the player has left and roll them (d6). 
+    The dice are then moved to the player's "table". The players cannot use the 
+    !roll command again until they use the !keep command.
+
+    Args
+    ctx - The message that triggered the command
+
+    Returns
+    sendStr - A string that the bot will send back to the player as feedback 
+    """
+    #Get the name of the author to compare to players
     rollerName = ctx.message.author.display_name
+    rollerMention = ctx.message.author.mention
     roller = None
+    
+    #Compare author name to player list
     for player in players:
-        if player.name == rollerName:
-            roller = player
+        if player.name == rollerName: #If author and player names match
+            roller = player #Author is the current roller
             break
+    
+    #If there is a roller in the player list
     if roller != None:
-      if roller.GetRollCount() > 0:
-        if(roller.GetChosen() == True):
+
+      if roller.GetRollCount() > 0: #If the player has rolls available
+        
+        if(roller.GetChosen() == True): #If the player has chosen since last roll
             roller.MakeRoll()
             dice.clear()
             strSend = roller.name + " rolled:\n"
-            for i in range(1, roller.GetRemainingDice()):
+            
+            for i in range(1, roller.GetRemainingDice()): #Roll only the table dice
               tmpRoll = random.randrange(1, 7)
               dice.append(tmpRoll)
-              roller.SetTableDice(tmpRoll)
+              roller.SetTableDice(tmpRoll) #Add the roll to player's table dice
               strSend += "#" + str(i) + ": " + str(dice[i - 1]) + "\n"
-            strSend += "Which dice would you like to keep?"
-        else:
+            
+            strSend += rollerMention + " which dice would you like to keep?"
+        
+        else: #If the player hasn't chosen since their last 
           strSend = "Don't get hasty! You need to choose from your last roll!"
-      else:
+      else: #If the player has made more than 3 rolls
         strSend = "You have reached your roll limit!"
-    else:
+    else: #If the author isn't a player
       strSend = ":x:" + rollerName + " is not a player:x:"
     await ctx.send(strSend)
 
 
 @bot.command()
 async def holds(ctx):
-    sendStr = ""
+    """A function that gets the player's held dice
+    Accesses and returns the player's GetHoldDice() function, 
+    Refer to playerClass.Player.GetHoldDice() for more info.
+
+    Args
+    ctx - The message that triggered the command
+
+    Returns
+    sendStr - A string that the bot will send back to the player as feedback 
+    """
+
+    #Get the name of the author and compare it to players
     holderName = ctx.message.author.display_name
     holderMention = ctx.message.author.mention
     holder = None
+    
+    #Compare author name to player list
     for player in players:
         if player.name == holderName:
             holder = player
             break
+    
+    #If there is a holder in the player list
     if holder != None:
       sendStr = holderMention + " you are holding:\n"
       sendStr += str(holder.GetHoldDice())
-    else:
+    
+    else: #If the author is not a player
       sendStr = ":x:" + holderName + " is not a player:x:"
     await ctx.send(sendStr)
 
 @bot.command()
 async def table(ctx):
-    sendStr = ""
+    """A function that gets the player's table dice
+    Accesses and returns the player's GetTableDice() function, 
+    Refer to playerClass.Player.GetTableDice() for more info.
+
+    Args
+    ctx - The message that triggered the command
+
+    Returns
+    sendStr - A string that the bot will send back to the player as feedback 
+    """
+    #Get the name of the author and compare it to players
     holderName = ctx.message.author.display_name
     holderMention = ctx.message.author.mention
     holder = None
+    
+    #Compare author name to player list
     for player in players:
         if player.name == holderName:
             holder = player
             break
+    
+    #If there is a holder in the player list
     if holder != None:
       sendStr = holderMention + " your table has:\n"
       sendStr += str(holder.GetTableDice())
@@ -91,6 +149,24 @@ async def table(ctx):
 
 @bot.command()
 async def keep(ctx, arg, die2=0, die3=0, die4=0, die5=0):
+    """A function that the player uses to choose which of the table dice they'll keep
+    Fetches a player's table dice and gives them the opportunity to choose
+    which dice (if any) they'll move to their held dice
+
+    Args
+    ctx - The message that triggered the command
+    
+    arg [Mandatory] [String OR int] - Used to choose any of the following:
+    - If the player keeps no dice
+    - If the player keeps all dice
+    - What die number (index) the player will keep
+
+    die1-5 - The die number (index) of the die they would like to keep
+
+    Returns
+    sendStr - A string that the bot will send back to the player as feedback 
+    """
+    
     chooserName = ctx.message.author.display_name
     chooser = None
     for player in players:
@@ -126,6 +202,17 @@ async def keep(ctx, arg, die2=0, die3=0, die4=0, die5=0):
 
 @bot.command()
 async def join(ctx):
+    """A function that has the player joining the list of players
+    Takes the author's display name and creates a player that uses that name
+    Checks if author's name is already in the list of players and makes sure that
+    players won't be registered twice
+
+    Args
+    ctx - The message that triggered the command
+
+    Returns
+    sendStr - A string that the bot will send back to the player as feedback 
+    """
     name = ctx.message.author.display_name
     isPlaying = False
     for player in players:
